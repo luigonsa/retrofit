a89c0978e5ea135445f3f893c04b8cd93cd7525b
basculasysistema@gmail.com <Lu1s@2015>
2019-04-21 21:06:26 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/apprecorridos/MainActivity.java
+++ app/src/main/java/com/example/apprecorridos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.apprecorridos;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +14,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.apprecorridos.interfaces.IFragments;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,IFragments {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,5 +100,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
